5b8840a1d22afb9e59fee7748fce64d2e75e9a62
YousefElbrolosy <yousefelbrolosy@gmail.com>
2023-08-21 10:31:20 +0200

    now displayed circuit

--- gametrial.py
+++ gametrial.py
@@ -1,2 +1,31 @@
 import pygame
-print("hello git")
+from controls.circuit_grid import CircuitGrid
+from model.circuit_grid_model import CircuitGridModel
+pygame.init()
+screen = pygame.display.set_mode((1185,700))
+pygame.display.set_caption('Brolos')
+clock = pygame.time.Clock()
+
+def main():
+    #initialise game 
+    circuit_grid_model = CircuitGridModel(3,16)
+    circuit_grid = CircuitGrid(0,475,circuit_grid_model)
+
+
+    exit = False
+    while not exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit = True
+
+        #update game
+
+        #draw
+        circuit_grid.draw(screen)
+        pygame.display.flip()
+
+        #set framerate
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
