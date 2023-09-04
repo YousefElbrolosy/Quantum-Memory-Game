59a00f06738d8bf9ee87488a37d3fbaabb1c910a
YousefElbrolosy <yousefelbrolosy@gmail.com>
2023-09-04 22:27:46 +0200

    selection border made round

--- card.py
+++ card.py
@@ -9,7 +9,9 @@ class Card():
         return self.card_img
     def set_border(self):
         self.border = True
-        pygame.draw.rect(self.back_img, (0,255,255), [0, 0,82.6446281 , 120], 5)
+        pygame.draw.rect(self.back_img, (0,255,255), [0, 0,82.6446281 , 120],5, 5)
+        #old
+        #pygame.draw.rect(self.back_img, (0,255,255), [0, 0,82.6446281 , 120], 5)
     def remove_border(self):
         self.border = False
         self.back_img = pygame.transform.scale(pygame.image.load('data/photos/yellow_back.png'),((82.6446281,120)))
